66c6f6328111717aa2e32e75eaa3c6253c90001a
AMRO GHNDAKJI <85643702+AMRO2017@users.noreply.github.com>
2022-02-05 18:27:55 +0300

    Add files via upload

---
+++ JS/loginScript.js
@@ -0,0 +1,9 @@
+'use strict';
+
+jQuery.noConflict();
+jQuery(document).ready(function ($) {
+
+    // usage: 2
+    $('#loginForm').formToJson('.result-json-output-login');
+
+});
---
+++ JS/signupScript.js
@@ -0,0 +1,10 @@
+'use strict';
+
+jQuery.noConflict();
+jQuery(document).ready(function ($) {
+
+    // usage: 2
+    $('#signupForm').formToJson('.result-json-output-signup');
+
+
+});
